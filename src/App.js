20fc2dc4cce1a409207dee3a8aae8974e6eca4fc
StephenWenceslao <stephenwenceslao@gmail.com>
2020-12-26 14:11:13 +0800

    Tried again

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>We now have auth</h1>
+        <h1>We now have auth!</h1>
       </header>
       <AmplifySignOut/>
     </div>
